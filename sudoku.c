2b4fd8cbe0e365a87944249dd963ff7052990764
Replit user <>
2023-10-05 03:37:08 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -61,7 +61,13 @@ int is_valid(Node* n){
   }
 
   for(int k = 0 ; k < 9 ; k++){
-    numerosValidos[k] = 1;
+    numerosValidos[k] = 0;
+  }
+
+  for(int i = 0 ; i < 9 ; i++){
+    for(int j = 0 ; j < 9 ; j++){
+      
+    }
   }
   
 

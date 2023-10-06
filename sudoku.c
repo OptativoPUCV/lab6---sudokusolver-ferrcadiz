9091b41cc4fedda18fb62cb2b24de7aa82abf595
Replit user <>
2023-10-06 00:09:18 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -112,6 +112,8 @@ List* get_adj_nodes(Node* n){
   for(int i = 0 ; i < 9 ; i++){
     for(int j = 0 ; j < 9 ; j++){
       if(n -> sudo[i][j] == 0){
+        int encontrado = 1;
+        
         for(int k = 1 ; k <= 9; k++){
           n -> sudo[i][j] = k;
 
@@ -124,6 +126,7 @@ List* get_adj_nodes(Node* n){
         return list;
       }
     }
+    if(encontrado) break;
   }
   return list;
 }
